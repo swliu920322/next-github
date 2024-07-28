7c9d489e334bd913f144d181f540bb2996f9b42b
liushengwei <1064084779@qq.com>
2024-07-28 11:16:23 +0800

    feat: fix git email

--- server/auth.mjs
+++ server/auth.mjs
@@ -3,38 +3,42 @@ import { config } from '../config.mjs';
 
 const { github: { client_id, client_secret, gitAccess, gitUser } } = config;
 export default (server) => {
-  server.use(async (ctx, next) => {
-    if (ctx.path === '/auth') {
-      const code = ctx.query.code;
-      if (!code) {
-        ctx.body = 'code not exist';
-        return;
-      }
-      const result = await axios({
-        method: 'POST',
-        url: gitAccess,
-        data: { client_id, client_secret, code },
-        headers: { Accept: 'application/json' },
-      });
-      if (result.status === 200) {
-        ctx.session.githubAuth = result.data;
-        const { access_token, token_type } = result.data;
-        const userInfoRes = await axios({
+  server
+    .use(async (ctx, next) => {
+      if (ctx.path === '/auth') {
+        const code = ctx.query.code;
+        if (!code) {
+          ctx.body = 'code not exist';
+          return;
+        }
+        const result = await axios({
           method: 'POST',
-          url: gitUser,
-          headers: {
-            Authorization: `${token_type} ${access_token}`,
-          },
+          url: gitAccess,
+          data: { client_id, client_secret, code },
+          headers: { Accept: 'application/json' },
         });
-        ctx.session.userInfo = userInfoRes.data;
-        ctx.redirect('/');
+        if (result.status === 200) {
+          ctx.session.githubAuth = result.data;
+          const { access_token, token_type } = result.data;
+          const userInfoRes = await axios({
+            method: 'POST',
+            url: gitUser,
+            headers: {
+              Authorization: `${token_type} ${access_token}`,
+            },
+          });
+          ctx.session.userInfo = userInfoRes.data;
+          ctx.redirect('/');
+        } else {
+          const errorMsg = result.data && result.data.error;
+          ctx.body = `request token failed ${errorMsg}`;
+        }
       } else {
-        const errorMsg = result.data && result.data.error;
-        ctx.body = `request token failed ${errorMsg}`;
+        await next();
       }
-    } else {
-      await next();
-    }
-
-  });
+    })
+    .use(async (ctx, next) => {
+      ctx.req.session = ctx.session;
+      await next()
+    });
 };
