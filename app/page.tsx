cd66db1162c2c1d9df1658edc487e3174e96cca5
liushengwei <1064084779@qq.com>
2024-07-30 18:02:38 +0800

    feat: add tab to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
-import { Button } from 'antd';
+import { Button, Tabs } from 'antd';
+import type { TabsProps } from 'antd';
 import { useAppSelector } from '@/lib/hooks';
 import { selectUserInfo } from '@/lib/features/user/userSlice';
 import { useDealLogin } from '@/lib/utils/Auth';
@@ -7,29 +8,49 @@ import { useCallback, useEffect, useState } from 'react';
 import { request } from '@/server/apiAgent.mjs';
 import { RepoItem } from '@/components/index/RepoItem';
 import { UserInfo } from '@/components/index/userInfo';
+import { useSearchParams, useRouter } from 'next/navigation';
 
 export default function Home() {
   const userInfo = useAppSelector(selectUserInfo);
+  const router = useRouter();
   const [repos, setRepos] = useState([]);
   const [starred, setStarred] = useState([]);
+  const [activeKey, setActiveKey] = useState('0');
   const { login } = useDealLogin();
   const getRepos = useCallback(() => {
-    request({ url: '/user/repos' })
-      .then(resp => {
-        setRepos(resp.data);
-      });
+    request({ url: '/user/repos' }).then(resp => setRepos(resp.data));
   }, []);
   const getStars = useCallback(() => {
-    request({ url: '/user/starred' })
-      .then(resp => {
-        setStarred(resp.data);
-      });
+    request({ url: '/user/starred' }).then(resp => setStarred(resp.data));
   }, []);
   
+  const searchParams = useSearchParams();
   useEffect(() => {
     getRepos();
     getStars();
+    const key = searchParams.get('query');
+    key && setActiveKey(key);
   }, []);
+  
+  const onChange = useCallback((key) => {
+    setActiveKey(key)
+    router.push(`/?query=${key}`);
+  }, []);
+  
+  const items: TabsProps['items'] = [
+    { label: '你的仓库', data: repos },
+    { label: '你关注的仓库', data: starred },
+  ].map((i, index) => {
+    return {
+      key: index + '',
+      label: i.label,
+      children: (
+        <div className="flex h-full flex-col gap-3 border-slate-500 border-t overflow-auto">
+          {i.data.map(item => <RepoItem key={item.id} item={item} />)}
+        </div>
+      ),
+    };
+  });
   if (!userInfo) {
     return (
       <div className="h-full flex justify-center items-center">
@@ -44,13 +65,9 @@ export default function Home() {
       <div className="h-full flex  p-6 gap-32 overflow-hidden">
         <UserInfo userInfo={userInfo} />
         <div className="flex-1 flex flex-col gap-1">
-          <span>user Repo</span>
-          <div className="flex flex-col gap-3 border-slate-500 border-t overflow-auto">
-            {repos.map(item => <RepoItem key={item.id} item={item} />)}
-          </div>
+          <Tabs activeKey={activeKey} onChange={onChange} className="h-full" items={items} />
         </div>
       </div>
     );
   }
-  
 }
