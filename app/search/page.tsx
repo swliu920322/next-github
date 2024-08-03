6eced9bae00612d5e07c11d718e28374a227184a
liushengwei <1064084779@qq.com>
2024-08-03 21:53:46 +0800

    feat: add pagination

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,17 +1,16 @@
 'use client';
 import { useEffect, useState } from 'react';
 import { request } from '@/server/apiAgent.mjs';
-import { List, Spin } from 'antd';
+import { List, Spin, Pagination } from 'antd';
 import Item from 'antd/lib/list/Item';
 import { RepoItem } from '@/components/index/RepoItem';
 import { useRouter } from 'next/navigation';
 import { getQueryStr } from '@/lib/utils/dealPathname';
 
-
 // 查询接口，需要处理一些字段
-function getData({ query, language, sort, order, page }) {
+function getData({ query, language, sort, order, page = 10 }) {
   const queryStr = getQueryStr({ query, language, sort, order, page }, true);
-  return request({ url: `/search/repositories${queryStr}` }).then(res => {
+  return request({ url: `/search/repositories${queryStr}` }).then((res) => {
     return res?.data || [];
   });
 }
@@ -26,25 +25,32 @@ const SORT_TYPES = [
 ];
 export default function Search({ searchParams }) {
   const [result, setRes] = useState({ items: [] });
+  let [loading, setLoading] = useState(true);
   const router = useRouter();
+
   useEffect(() => {
-    getData(searchParams).then(res => {
-      setRes(res);
-    });
+    setLoading(true);
+    getData(searchParams)
+      .then((res) => setRes(res))
+      .catch((err) => {
+        console.log(err);
+        setRes({ items: [] });
+      })
+      .finally(() => setLoading(false));
   }, [searchParams]);
-  
+
   function handleLanguage(item) {
     const queryStr = getQueryStr({ ...searchParams, language: item });
     router.push(`/search${queryStr}`);
     setRes({ items: [] });
   }
-  
+
   function handleOrder(item) {
     const queryStr = getQueryStr({ ...searchParams, sort: item.value, order: item.order });
     router.push(`/search${queryStr}`);
     setRes({ items: [] });
   }
-  
+
   function dealSort(item) {
     const { sort, order } = searchParams;
     if (item.value) {
@@ -52,7 +58,12 @@ export default function Search({ searchParams }) {
     }
     return !item.value && !sort;
   }
-  
+
+  function pageChange(page: Number) {
+    const queryStr = getQueryStr({ ...searchParams, page });
+    router.push(`/search${queryStr}`);
+  }
+
   return (
     <div className="flex p-4 overflow-hidden h-full">
       <div className="flex w-40 flex-col overflow-auto h-full">
@@ -60,35 +71,32 @@ export default function Search({ searchParams }) {
           header={<span>语言</span>}
           bordered
           dataSource={languages}
-          renderItem={(item) =>
-            <Item
-              className={`cursor-pointer ${item === searchParams.language && 'border-l-2 border-blue-400 active-item'}`}
-              onClick={() => item !== searchParams.language && handleLanguage(item)}>
+          renderItem={(item) => (
+            <Item className={`cursor-pointer ${item === searchParams.language && 'border-l-2 border-blue-400 active-item'}`} onClick={() => item !== searchParams.language && handleLanguage(item)}>
               {item}
             </Item>
-          }
+          )}
         />
         <List
           header={<span>排序</span>}
           bordered
           dataSource={SORT_TYPES}
-          renderItem={(item) =>
-            <Item className={`cursor-pointer ${dealSort(item) && 'border-l-2 border-blue-400 active-item'}`}
-                  onClick={() => !dealSort(item) && handleOrder(item)}>
+          renderItem={(item) => (
+            <Item className={`cursor-pointer ${dealSort(item) && 'border-l-2 border-blue-400 active-item'}`} onClick={() => !dealSort(item) && handleOrder(item)}>
               {item.name}
             </Item>
-          }
+          )}
         />
       </div>
-      <div className="flex-1 ml-4 overflow-hidden h-full">
-        <div className="flex h-full flex-col gap-3 border-slate-500 border-t overflow-auto">
-          {
-            result?.items ?
-              result?.items?.map(item => <RepoItem key={item.id} item={item} />) :
-              <div className="w-full h-full flex justify-center items-center">
-                <Spin />
-              </div>
-          }
+      <div className="flex-1 flex flex-col ml-4 overflow-hidden h-full relative">
+        <div className="flex flex-1 flex-col gap-3 border-slate-500 border-t overflow-auto">{result?.items?.map((item) => <RepoItem key={item.id} item={item} />)}</div>
+        {loading && (
+          <div className="bg-grey-300 absolute left-0 top-0 w-full h-full flex justify-center items-center">
+            <Spin />
+          </div>
+        )}
+        <div className="mt-2 self-center">
+          <Pagination pageSize={10} showSizeChanger={false} current={Number(searchParams.page) || 1} total={result?.total_count || 0} onChange={pageChange} />
         </div>
       </div>
     </div>
--- lib/utils/dealPathname.ts
+++ lib/utils/dealPathname.ts
@@ -5,8 +5,8 @@ function dealCode(str = '') {
 //  获取路由接口
 //  url上一直都是 ?a=b&b=c...
 //  重新设计，只有提交api的时候进行query => q, &language= => +language:
-export function getQueryStr(item: Record<string, string>, isRequest = false) {
-  const { query, language, sort, order = 'desc', page } = item;
+export function getQueryStr(item: Record<string, string | number>, isRequest = false) {
+  const { query, language, sort, order = 'desc', page = 10 } = item;
   let queryStr = `?${isRequest ? 'q' : 'query'}=${query}`;
   if (language) {
     queryStr += `${isRequest ? '+' : '&'}language${isRequest ? ':' : '='}${language}`;
