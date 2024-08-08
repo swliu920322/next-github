02bdab3e8ae5c67854ba017ea23332ef0020c196
liushengwei <1064084779@qq.com>
2024-08-09 00:02:44 +0800

    feat: fix some error

--- app/detail/issues/comp-issues.tsx
+++ app/detail/issues/comp-issues.tsx
@@ -1,21 +1,21 @@
 import { Avatar } from 'antd';
+import moment from 'moment';
 
-function IssueItem(issue) {
+function IssueItem({ issue }) {
   return (
-    <div className="flex items-center gap-6">
+    <div className="flex items-center gap-6 border-b-2 border-blue-300 pb-2">
       <Avatar src={issue?.user?.avatar_url} size={50} shape="square" />
       <div className="flex flex-col gap-2">
         <h6>{issue.title}</h6>
-        <span>Update at {issue.update_at}</span>
+        <span>Update at {moment(issue.updated_at).fromNow()}</span>
       </div>
     </div>
   );
 }
 
 export function CompIssues({ issues }) {
-  console.log(issues);
   return (
-    <div>
+    <div className="flex flex-col gap-3 ">
       {issues.map((issue) => (
         <IssueItem key={issue.id} issue={issue} />
       ))}
