98573a5847130f16b7e4db866251a50abbc04518
liushengwei <1064084779@qq.com>
2024-08-08 23:39:01 +0800

    feat: start to finish detail page with issues

--- app/detail/issues/page.tsx
+++ app/detail/issues/page.tsx
@@ -1,3 +1,22 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { request } from '@/server/apiAgent.mjs';
+
 export default function Page() {
-  return <div>issue</div>
+  const searchParams = useSearchParams();
+  const owner = searchParams.get('owner');
+  const name = searchParams.get('name');
+  const [issues, setIssues] = useState([])
+  const [labels, setLabels] = useState([])
+  useEffect(() => {
+    request({ url: `/repos/${owner}/${name}/issues` }).then((resp) => {
+      console.log(resp.data);
+    });
+    request({ url: `/repos/${owner}/${name}/labels` }).then((resp) => {
+      console.log(resp.data);
+    });
+  }, [owner, name]);
+  return <div>issue</div>;
 }
