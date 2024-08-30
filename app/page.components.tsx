a08c57a391f5e44a746083624b1096f86a1edbb9
liushengwei <1064084779@qq.com>
2024-08-30 22:46:05 +0800

    fix: remove unnecessary useEffect

--- app/page.components.tsx
+++ app/page.components.tsx
@@ -2,7 +2,7 @@
 import { Button, Tabs, TabsProps } from 'antd';
 import { useDealLogin } from '@/lib/utils/Auth';
 import { RepoItem } from '@/components/index/RepoItem';
-import { useCallback, useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { getList } from '@/app/page.action';
 import useSWR from 'swr';
@@ -25,15 +25,15 @@ export function TabSelect() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const { data, isLoading } = useSWR('getUserRepo', getList);
-  useEffect(() => {
-    const key = searchParams.get('tab');
-    setActiveKey(key || '0');
-  }, [searchParams]);
+  // useEffect(() => {
+  const key = searchParams.get('tab');
+  setActiveKey(key || '0');
+  // }, [searchParams]);
 
-  const onChange = useCallback((key: string) => {
+  const onChange = (key: string) => {
     setActiveKey(key);
     router.push(`/?tab=${key}`);
-  }, []);
+  };
 
   const items: TabsProps['items'] = [
     { label: '你的仓库', data: data?.repos || [] },
